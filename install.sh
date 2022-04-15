98a7f6eba056014c7ff563bb2c7bb5457cf7f598
Сергей <32985830+vasilievsv@users.noreply.github.com>
2022-04-15 20:14:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# setup ubuntu environment
+sudo apt-get install -Y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath \
+socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping libsdl1.2-dev xterm
+
 git -C ./layers/bsp clone -b dunfell https://github.com/linux-sunxi/meta-sunxi
 git -C ./layers clone -b dunfell git://git.yoctoproject.org/poky
 git -C ./layers/third-party clone -b dunfell https://github.com/openembedded/meta-openembedded
